0b1dff6c8e1cff1d08b0e92e1b97b58eb1a598ea
jonghunlee <whdcn@naver.com>
2017-11-04 23:16:09 +0900

    Bottom Sheet 적용

--- app/src/main/java/io/mashup/exit11/MainActivity.java
+++ app/src/main/java/io/mashup/exit11/MainActivity.java
@@ -1,16 +1,27 @@
 package io.mashup.exit11;
 
+import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.BottomSheetBehavior;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
+import static android.support.design.widget.BottomSheetBehavior.STATE_COLLAPSED;
+import static android.support.design.widget.BottomSheetBehavior.STATE_SETTLING;
+
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.layout_content)
-    FrameLayout flContentl;
+    FrameLayout flContent;
+
+    @BindView(R.id.layout_bottom_sheet)
+    ConstraintLayout clBottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +29,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
+
+        BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(clBottomSheet);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case STATE_COLLAPSED:
+                        break;
+                    case STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
     }
 }
