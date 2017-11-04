cff942fb5ab4e58b58c56ad0137ed0d529ec1c4d
jonghunlee <whdcn@naver.com>
2017-11-04 21:20:47 +0900

    Fragment 영역 설정

--- app/src/main/java/io/mashup/exit11/MainActivity.java
+++ app/src/main/java/io/mashup/exit11/MainActivity.java
@@ -2,12 +2,21 @@ package io.mashup.exit11;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.layout_content)
+    FrameLayout flContentl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
 }
