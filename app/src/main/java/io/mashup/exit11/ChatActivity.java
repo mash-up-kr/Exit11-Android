5ab8c8a54498fde06d7071449be50b38cefc279f
Tak <tak8997@nate.com>
2017-11-05 00:02:27 +0900

    ChatActivity 생성. DrawerLayout추가

---
+++ app/src/main/java/io/mashup/exit11/ChatActivity.java
@@ -0,0 +1,40 @@
+package io.mashup.exit11;
+
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+
+public class ChatActivity extends AppCompatActivity {
+    DrawerLayout drawer;
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawer.openDrawer(Gravity.END);
+            }
+        });
+        drawer = (DrawerLayout)findViewById(R.id.drawer_layout);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.END)) {
+            drawer.closeDrawer(GravityCompat.END);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+
+}
