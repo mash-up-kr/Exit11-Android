fc892eb8ff42f706156fa7a8ad9bc37f9fe3414c
jonghunlee <whdcn@naver.com>
2018-01-17 17:15:19 +0900

    2018. 01. 17 에뮬레이터 테스트용 구글맵 API 추가

--- app/src/main/java/io/mashup/exit11/SplashActivity.java
+++ app/src/main/java/io/mashup/exit11/SplashActivity.java
@@ -1,17 +1,16 @@
 package io.mashup.exit11;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 
-import io.mashup.exit11.ui.activity.LoginActivity;
 import io.mashup.exit11.ui.activity.MainActivity;
 
 public class SplashActivity extends Activity {
+
     private final int SPLASH_TIME_OUT = 3000;
 
     @Override
@@ -19,17 +18,9 @@ public class SplashActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-
         ImageView iv = findViewById(R.id.splash_imageView);
 
-            Glide.with(this)
-                    .asGif()
-                    .load(R.raw.splash2)
-                    .into(iv);
-
-//        Glide.with(SplashActivity.this)
-//                .load(R.raw.splash)
-//                .into(imageViewTarget);
+        Glide.with(this).asGif().load(R.raw.splash2).into(iv);
 
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -41,8 +32,8 @@ public class SplashActivity extends Activity {
 
     //change
     private void startMainActivity() {
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
+        //        Intent intent = new Intent(this, LoginActivity.class);
+        MainActivity.start(this);
         finish();
     }
 }
--- app/src/main/java/io/mashup/exit11/ui/activity/MainActivity.java
+++ app/src/main/java/io/mashup/exit11/ui/activity/MainActivity.java
@@ -1,84 +1,28 @@
 package io.mashup.exit11.ui.activity;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.Toast;
 
 import butterknife.ButterKnife;
 import io.mashup.exit11.R;
-import io.mashup.exit11.ui.fragment.MapFragment;
-import io.mashup.exit11.util.CommonUtil;
-import io.mashup.exit11.util.PermissionUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private static final int MESSAGE_BACKKEY_TIMEOUT = 2;
-    private static final long TIMEOUT_BACKKEY_DELAY = 2000;
-    private boolean isBackPressed = false;
+    public static void start(Context context) {
+        context.startActivity(new Intent(context, MainActivity.class));
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         ButterKnife.bind(this);
-        if (PermissionUtils.checkPermissions(this,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION) && CommonUtil.isNetworkWorking(this)) {
 
-            getSupportFragmentManager().beginTransaction()
-                    .replace(R.id.layout_content, MapFragment.newInstance())
-                    .commit();
-        }
-        else {
-            PermissionUtils.requestPermissions(this,
-                    0,
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    Manifest.permission.ACCESS_COARSE_LOCATION);
-        }
     }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case 0:
-                getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.layout_content, MapFragment.newInstance())
-                        .commit();
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-
-        if (!isBackPressed) {
-            isBackPressed = true;
-            Toast.makeText(this, "한번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
-            backKeyHandler.sendEmptyMessageDelayed(MESSAGE_BACKKEY_TIMEOUT, TIMEOUT_BACKKEY_DELAY);
-        }
-        else {
-            backKeyHandler.removeMessages(MESSAGE_BACKKEY_TIMEOUT);
-            super.onBackPressed();
-        }
-    }
-
-    @SuppressLint("HandlerLeak")
-    private Handler backKeyHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case MESSAGE_BACKKEY_TIMEOUT:
-                    isBackPressed = false;
-                    break;
-            }
-        }
-    };
 }
